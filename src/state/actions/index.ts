b1958199f22eb86256107d8d09bc7833c06fd631
mdashikadnan <md.ashikadnan@gmail.com>
2022-07-23 15:58:11 +1000

    separated actions and action type

---
+++ src/state/actions/index.ts
@@ -0,0 +1,20 @@
+import {ActionType} from "../action-types";
+
+//Place different kinds of actions inside this file
+
+interface SearchRepositoriesAction {
+    type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+    type: ActionType.SEARCH_REPOSITORIES_ERROR;
+    payload: string;
+}
+export type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
+//Very closely related definition or meaning
+//Provide all different action types
