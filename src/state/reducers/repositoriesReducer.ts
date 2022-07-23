6efdb9e88d3c114e697653fa6fdf828c1f385b4c
mdashikadnan <md.ashikadnan@gmail.com>
2022-07-23 15:38:45 +1000

    Apply separate action interfaces

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,13 +1,20 @@
-interface RepositoriesState {
+  interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
   }
-  interface Action {
-    type: string;
-    payload?:any;
+  interface SearchRepositoriesAction{
+    type: 'search_repositories';
   }
-  const reducer = (state: RepositoriesState, action: Action) : RepositoriesState=> {
+  interface SearchRepositoriesSuccessAction{
+    type: 'search_repositories_success';
+    payload: string[];
+  }
+  interface SearchRepositoriesErrorAction{
+    type: 'search_repositories_error';
+    payload: string;
+  }
+  const reducer = (state: RepositoriesState, action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction) : RepositoriesState=> {
     switch (action.type) {
       case 'search_repositories':
         return { loading: true, error: null, data: [] };
